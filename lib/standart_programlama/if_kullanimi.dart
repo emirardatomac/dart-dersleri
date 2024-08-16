b463bf911a259185cd096bb36beeec9965878e8b
Emir Arda Tomac <144858927+emirardatomac@users.noreply.github.com>
2024-08-16 06:22:40 +0300

    if kullanimi

---
+++ lib/standart_programlama/if_kullanimi.dart
@@ -0,0 +1,17 @@
+void main(){
+  int yas = 21;
+  String isim = "Emir";
+
+  if(yas >= 18){
+    print("Reşitsiniz");
+  }else{
+    print("Reşit değilsiniz.");
+  }
+  if(isim == "Emir" ){
+    print("Merhaba Emir ! ");
+  }else if(isim == "Arda"){
+    print("Merhaba Arda ! ");
+  }else{
+    print("Tanınmayan kişi ! ");
+  }
+}
