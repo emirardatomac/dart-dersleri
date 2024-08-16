4a0a8c10a86412258fa4e5f3869c710c32ef4e56
Emir Arda Tomac <144858927+emirardatomac@users.noreply.github.com>
2024-08-16 16:15:25 +0300

    Fonksiyonlar

--- lib/nesne_tabanli_programlama/fonksiyonlar.dart
+++ lib/nesne_tabanli_programlama/fonksiyonlar.dart
@@ -4,4 +4,20 @@ class Fonksiyonlar {
     String sonuc = "Merhaba Emir";
     print(sonuc);
   }
+  //return : geri dönüş değeri olan / veri aktarırım
+String selamla2(){
+    String sonuc = "Merhaba Emir";
+    return sonuc;
+}
+//parametre: dışardan veri almak
+  void selamla3 (String isim){
+    String sonuc = "Merhaba $isim ";
+    print(sonuc);
+  }
+  int toplama(int sayi1,int sayi2){
+    int toplam = sayi1 + sayi2;
+    return toplam;
+
+  }
+
 }
--- lib/nesne_tabanli_programlama/fonksiyonlar_main.dart
+++ lib/nesne_tabanli_programlama/fonksiyonlar_main.dart
@@ -1,3 +1,17 @@
+import 'package:dart_dersleri/nesne_tabanli_programlama/fonksiyonlar.dart';
+
 void main(){
+  var f = Fonksiyonlar();
+
+  f.selaml1();
+
+  String gelenSonuc = f.selamla2();
+  print("Gelen sonuç : $gelenSonuc");
+
+  f.selamla3("Emir");
+
+  int gelenToplam = f.toplama(1, 2);
+  print(gelenToplam);
+
 
 }
