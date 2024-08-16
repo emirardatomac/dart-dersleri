25b18d11cddc0f1c0d7bdc0695e7e3d579c3d926
Emir Arda Tomac <144858927+emirardatomac@users.noreply.github.com>
2024-08-16 16:29:52 +0300

    İç Açılar -ic carpım yanlış yazılmıştır..-

---
+++ lib/odev1/icCarpim.dart
@@ -0,0 +1,8 @@
+class icCarpim {
+  double hesapla(int kenarSayisi){
+    double icCarpimtoplami = ((kenarSayisi-2)*180)/kenarSayisi;
+    return icCarpimtoplami;
+
+  }
+
+}
---
+++ lib/odev1/icCarpim_main.dart
@@ -0,0 +1,9 @@
+import 'package:dart_dersleri/odev1/icCarpim.dart';
+
+void main(){
+  var f = icCarpim();
+
+  double gelenCarpim = f.hesapla(5);
+  print(gelenCarpim);
+
+}
