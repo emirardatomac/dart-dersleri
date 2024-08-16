c56376f2d20145cf462c076740de88f3eb5e0e03
Emir Arda Tomac <144858927+emirardatomac@users.noreply.github.com>
2024-08-16 05:55:14 +0300

    Degisken oluşturma

---
+++ lib/degiskenler/degisken_olusturma.dart
@@ -0,0 +1,29 @@
+void main(){
+  var ogrenciAdi = "Emir";
+  var ogrenciYas = 21 ;
+  var ogrenciBoy = 1.90 ;
+  var ogrenciBasharf = "E" ;
+  var ogrenciDevamEdiyorMu = true;
+
+  print(ogrenciAdi);
+  print(ogrenciYas);
+  print(ogrenciBoy);
+  print(ogrenciBasharf);
+  print(ogrenciDevamEdiyorMu);
+
+  int urun_id = 123;
+  String urun_adi = "Kol Saati";
+  int urun_adet = 100;
+  double urun_fiyati = 146.99;
+  String urun_tedarikci = "Casio";
+
+  print("Ürün id : $urun_id");
+  print("Ürün adı : $urun_adi");
+  print("Ürün adet : $urun_adet");
+  print("Ürün fiyatı : $urun_fiyati");
+  print("Ürün tedarikçi : $urun_tedarikci");
+
+  int a = 100;
+  int b = 200;
+  print("$a ve $b nin toplamı : ${a+b}");
+}
