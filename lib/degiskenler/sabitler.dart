69edcd8b062e986e4a7b00556e06febcc1fddcf6
Emir Arda Tomac <144858927+emirardatomac@users.noreply.github.com>
2024-08-16 06:02:59 +0300

    sabitler

---
+++ lib/degiskenler/sabitler.dart
@@ -0,0 +1,21 @@
+void main(){
+//sabitler
+//final,const
+int sayi = 30;
+print(sayi);
+sayi = 100;
+print(sayi);
+
+//kodlamayı çalıştırdığımızda hafızada çalışır.
+final int numara = 100;
+print(numara);
+//numara = 200;
+
+  //değişkeni tanımladığımızda hafızada oluşur
+  //görsel nesnelerde kullanırız.
+  const int numara2 = 100;
+  print(numara2);
+//const = 200;
+
+
+}
