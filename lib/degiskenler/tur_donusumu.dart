49e1cead6ddf8aed30d7ce67edd2e5752369c6f6
Emir Arda Tomac <144858927+emirardatomac@users.noreply.github.com>
2024-08-16 06:10:46 +0300

    tur donusumleri

---
+++ lib/degiskenler/tur_donusumu.dart
@@ -0,0 +1,23 @@
+void main(){
+  //Sayısaldan sayısala dönüşüm
+  int i = 56;
+  double d = 78.67;
+
+  int sonuc1 = d.toInt();
+  double sonuc2 = i.toDouble();
+  print(sonuc1);
+  print(sonuc2);
+
+  //Sayısaldan metine dönüşüm
+  String sonuc3 = i.toString();//"56"
+  String sonuc4 = d.toString();//"78.67"
+  print(sonuc3);
+  print(sonuc4);
+
+  //Metinden sayısala dönüşüm
+  String yazi1 = "25";
+  String yazi2 = "51.54";
+
+  int sonuc5 = int.parse(yazi1);
+  double sonuc6 = double.parse(yazi2);
+}
