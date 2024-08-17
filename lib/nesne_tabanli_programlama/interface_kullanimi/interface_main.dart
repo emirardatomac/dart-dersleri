0ff88922b0f090f3813272392d9d54b945adf17b
Emir Arda Tomac <144858927+emirardatomac@users.noreply.github.com>
2024-08-17 04:20:24 +0300

    interface

---
+++ lib/nesne_tabanli_programlama/interface_kullanimi/interface_main.dart
@@ -0,0 +1,9 @@
+import 'package:dart_dersleri/nesne_tabanli_programlama/interface_kullanimi/class_a.dart';
+
+void main(){
+  var a = ClassA();
+  print(a.degisken);
+  a.metod1();
+  print(a.metod2());//Return olduğu için.
+
+}
